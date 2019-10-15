cf02dd8204cd879cdb63e33660ea3bab820a489d
enilu <eniluz@qq.com>
2019-10-15 17:15:58 +0800

    Update 升级element-ui和vue全家桶

--- flash-waimai-manage/build/webpack.base.conf.js
+++ flash-waimai-manage/build/webpack.base.conf.js
@@ -9,16 +9,6 @@ function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
 
-// const createLintingRule = () => ({
-//   test: /\.(js|vue)$/,
-//   loader: 'eslint-loader',
-//   enforce: 'pre',
-//   include: [resolve('src'), resolve('test')],
-//   options: {
-//     formatter: require('eslint-friendly-formatter'),
-//     emitWarning: !config.dev.showEslintErrorsInOverlay
-//   }
-// })
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
