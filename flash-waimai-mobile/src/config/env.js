082a374e0d17ac176640418c0dac1340d6727fda
microapp <eniluzt@qq.com>
2021-04-22 21:16:49 +0800

    Change readme

--- flash-waimai-mobile/src/config/env.js
+++ flash-waimai-mobile/src/config/env.js
@@ -14,6 +14,7 @@ let imgBaseUrl = '';
 
 if (process.env.NODE_ENV == 'development') {
   imgBaseUrl = '/file/getImgStream?fileName='
+  baseUrl = 'http://localhost:8082'
 } else if (process.env.NODE_ENV == 'production') {
   baseUrl = '//waimai-api.microapp.store';
   imgBaseUrl = '//waimai-api.microapp.store/file/getImgStream?fileName=';
